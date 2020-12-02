6ff2142b13fc08a38000a4be04d6c63ee53d7da1
totetmatt <matthieu.totet@gmail.com>
2020-12-02 08:11:38 +0100

    One loop

--- scala/src/main/scala/Day02.scala
+++ scala/src/main/scala/Day02.scala
@@ -6,24 +6,21 @@ object Day02 extends App {
   val passwords = s.getLines.toSeq
 
   val parsePattern = "([0-9]+)-([0-9]+) ([a-z]): ([a-z]+)".r
-  val test = "13-16 f: rfjmkrqkqrxmfnqj"
 
-  val parsePattern(min,max, checkLetter, password) = test
+  val Seq(check1:Int,check2:Int) = passwords.foldLeft(Seq(0, 0))((agg, el) => {
+    val parsePattern(n1, n2, ruleLetter, password) = el
+    Seq(
+      agg.head + check1(n1.toInt, n2.toInt, ruleLetter.head, password),
+      agg.last + check2(n1.toInt - 1, n2.toInt - 1, ruleLetter.head, password)
+    )
+  })
 
+  def check1(min: Int, max: Int, ruleLetter: Char, password: String): Int = if (min <= password.toSeq.count(_ == ruleLetter) && password.toSeq.count(_ == ruleLetter) <= max) 1 else 0
 
-  println(passwords
-    .count{ x=>
-      val parsePattern(min,max, ruleLetter, password) = x
-      min.toInt <= password.toSeq.count(_ == ruleLetter.head) && password.toSeq.count(_ == ruleLetter.head) <= max.toInt
-    })
-
-  println(passwords
-    .count{ x=>
-      val parsePattern(pos1,pos2, ruleLetter, password) = x
-      val check = Set(password(pos1.toInt-1),password(pos2.toInt-1))
-      check.size == 2 && check.contains(ruleLetter.head)
-    })
-
-  s.close()
+  def check2(pos1: Int, pos2: Int, ruleLetter: Char, password: String): Int = {
+    val check = Set(password(pos1), password(pos2))
+    if (check.size == 2 && check.contains(ruleLetter)) 1 else 0
+  }
+  println(s"Check 1 count:${check1} \nCheck 2 count:${check2}")
 
 }
